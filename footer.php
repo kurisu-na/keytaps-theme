f534cd2cf347cd61a55d6390dd22e881afd983f8
Christina Natasha <kurisu.natasha25@gmail.com>
2021-04-28 07:27:52 +0700

    Update to v1.2

--- footer.php
+++ footer.php
@@ -17,19 +17,30 @@
     <?php get_template_part( 'footer-widget' ); ?>
 	<footer id="colophon" class="site-footer <?php echo wp_bootstrap_starter_bg_class(); ?>" role="contentinfo">
 		<div class="container pt-3 pb-3">
-			<div class="navbar-brand justify-content-center">
-				<?php if ( get_theme_mod( 'wp_bootstrap_starter_logo' ) ): ?>
-					<a href="<?php echo esc_url( home_url( '/' )); ?>">
+			<?php if ( get_theme_mod( 'wp_bootstrap_starter_logo' ) ): ?>
+				<div class="navbar-brand justify-content-center">
+					<a href="https://christinanatasha.me">
 						<img src="<?php echo esc_url(get_theme_mod( 'wp_bootstrap_starter_logo' )); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
 					</a>
-				<?php else : ?>
-					<a class="site-title" href="<?php echo esc_url( home_url( '/' )); ?>"><?php esc_url(bloginfo('name')); ?></a>
-				<?php endif; ?>
-			</div>
+				</div>
+			<?php endif; ?>
             <div class="site-info">
-                Design and content &copy; <?php echo date('Y'); ?> <?php echo '<a href="https://christinanatasha.me/">Christina Natasha</a>'; ?>
-                <span class="sep"> | </span>
-                <a class="credits" href="https://afterimagedesigns.com/wp-bootstrap-starter/" target="_blank" title="WordPress Technical Support" alt="Bootstrap WordPress Theme"><?php echo esc_html__('Based on the Bootstrap WordPress Theme','wp-bootstrap-starter'); ?></a>
+                <p>
+					Design and content &copy; <?php echo date('Y'); ?> <?php echo '<a href="https://christinanatasha.me">Christina Natasha</a>'; ?>
+				</p>
+				<p>
+					Connect with me! <br>
+                	<a href="https://www.linkedin.com/in/kurisunatasha25">LinkedIn</a>
+					<span class="sep"> | </span>
+					<a href="https://bit.ly/kurisu-upwork-profile">Upwork</a>
+					<span class="sep"> | </span>
+					<a href="https://www.fiverr.com/kurisu_na">Fiverr</a>
+					<span class="sep"> | </span>
+					<a href="https://www.behance.net/kurisu_na">Behance</a>
+                </p>
+				<p>
+					<a class="credits" href="https://afterimagedesigns.com/wp-bootstrap-starter/" target="_blank" title="WordPress Technical Support" alt="Bootstrap WordPress Theme"><?php echo esc_html__('Based on the Bootstrap WordPress Theme','wp-bootstrap-starter'); ?></a>
+				</p>
 
             </div><!-- close .site-info -->
 		</div>
